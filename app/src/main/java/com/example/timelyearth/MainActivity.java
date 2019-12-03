1d99989022cefeb0494b1159f9bc8dfcbc1661e3
Maisha Nabila <kmaisha@outlook.com>
2019-12-03 18:19:59 -0500

    Timer Done

--- app/src/main/java/com/example/timelyearth/MainActivity.java
+++ app/src/main/java/com/example/timelyearth/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.timelyearth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
+    private Chronometer chronometer;
+    private long offsetPause;
+    private boolean running;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.projects);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openProjectsActivity();
+            }
+
+        });
+        chronometer = findViewById(R.id.chronometer);
+    }
+    private void openProjectsActivity() {
+        Intent intent = new Intent(this, Projects.class);
+        startActivity(intent);
+    }
+    public void startChronometer(View v){
+        if(!running){
+            chronometer.setBase(SystemClock.elapsedRealtime() - offsetPause);
+            chronometer.start();
+            running = true;
+        }
+    }
+    public void pauseChronometer(View v){
+        if(running){
+            chronometer.stop();
+            offsetPause = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
+        }
+    }
+    public void resetChronometer(View v){
+        chronometer.stop();
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        offsetPause = 0;
+        running = false;
+
     }
 }
